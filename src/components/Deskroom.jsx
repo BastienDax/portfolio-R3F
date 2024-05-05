c3b9b8bb8a8f50eecd7260c51a54a8a3be5169cc
Bastien Daxhelet <bastos@MacBook-Pro-de-Bastien.local>
2024-05-05 12:30:04 -0400

    add overlay

--- src/components/Deskroom.jsx
+++ src/components/Deskroom.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { useGLTF, useTexture } from "@react-three/drei";
 import * as THREE from "three";
+import { useThree } from "@react-three/fiber";
 
 export function Deskroom() {
   const { nodes } = useGLTF("models/deskroom.glb");
 
   const texture = useTexture("models/deskroom_texture.webp");
 
+  const { viewport } = useThree();
+
   texture.flipY = false;
   texture.colorSpace = THREE.SRGBColorSpace;
 
+  const isMobile = window.innerWidth < 768;
+  const responsiveRatio = viewport.width / 12;
+
   return (
-    <mesh geometry={nodes.baked.geometry}>
+    <mesh
+      scale={isMobile ? responsiveRatio : 1}
+      geometry={nodes.baked.geometry}
+    >
       <meshBasicMaterial map={texture} />
     </mesh>
   );
--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,6 +1,5 @@
 import { useProgress } from "@react-three/drei";
 import { usePlay } from "../contexts/Play";
-import { Link } from "react-router-dom";
 
 export const Overlay = () => {
   const { progress } = useProgress();
@@ -17,32 +16,43 @@ export const Overlay = () => {
       {progress === 100 && (
         <>
           <div className={`intro ${play ? "intro--disappear" : ""}`}>
-            <h1 className="logo">
-              <b>Bastien Daxhelet</b> — Développeur Créatif
+            <h1 className="logo top-[45vh]">
+              <b>Bastien Daxhelet</b> <br className="md:hidden" />—
+              <br className="md:hidden" /> Développeur Créatif
             </h1>
             <p className="intro__scroll">Scroll pour découvrir mon univers</p>
-            <button
-              className="explore"
-              onClick={() => {
-                setPlay(true);
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="13"
-                height="13"
-                viewBox="0 0 13 13"
-                fill="none"
+            <div className="flex items-center justify-center flex-col gap-y-4 mt-16 md:mt-0 md:gap-y-0">
+              <button
+                className="explore"
+                onClick={() => {
+                  setPlay(true);
+                }}
               >
-                <path d="M1.8418 12.3999C1.57324 12.3999 1.3584 12.3022 1.19727 12.1069C1.04102 11.9165 0.962891 11.6626 0.962891 11.3452V1.04736C0.962891 0.72998 1.04102 0.476074 1.19727 0.285645C1.3584 0.0952148 1.57324 0 1.8418 0C1.98828 0 2.12744 0.0268555 2.25928 0.0805664C2.39111 0.129395 2.53027 0.195312 2.67676 0.27832L11.2168 5.22217C11.5244 5.39795 11.7368 5.55664 11.854 5.69824C11.9761 5.83496 12.0371 6.00098 12.0371 6.19629C12.0371 6.3916 11.9761 6.56006 11.854 6.70166C11.7368 6.83838 11.5244 6.99707 11.2168 7.17773L2.67676 12.1143C2.53027 12.2021 2.39111 12.2705 2.25928 12.3193C2.12744 12.373 1.98828 12.3999 1.8418 12.3999Z" />
-              </svg>
-              Démarrer l'expérience
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="13"
+                  height="13"
+                  viewBox="0 0 13 13"
+                  fill="none"
+                >
+                  <path d="M1.8418 12.3999C1.57324 12.3999 1.3584 12.3022 1.19727 12.1069C1.04102 11.9165 0.962891 11.6626 0.962891 11.3452V1.04736C0.962891 0.72998 1.04102 0.476074 1.19727 0.285645C1.3584 0.0952148 1.57324 0 1.8418 0C1.98828 0 2.12744 0.0268555 2.25928 0.0805664C2.39111 0.129395 2.53027 0.195312 2.67676 0.27832L11.2168 5.22217C11.5244 5.39795 11.7368 5.55664 11.854 5.69824C11.9761 5.83496 12.0371 6.00098 12.0371 6.19629C12.0371 6.3916 11.9761 6.56006 11.854 6.70166C11.7368 6.83838 11.5244 6.99707 11.2168 7.17773L2.67676 12.1143C2.53027 12.2021 2.39111 12.2705 2.25928 12.3193C2.12744 12.373 1.98828 12.3999 1.8418 12.3999Z" />
+                </svg>
+                Démarrer l'expérience
+              </button>
+              <button
+                className="contact-mobile md:hidden"
+                onClick={() => {
+                  setPlay(true);
+                }}
+              >
+                Me contacter
+              </button>
+            </div>
           </div>
-          <div className="absolute top-8 left-8">
+          <div className="absolute top-8 left-1/2 -translate-x-1/2 md:left-8 md:-translate-x-0">
             <img className="h-20" src="logo/logo_blanc.svg" alt="" />
           </div>
-          <div className="absolute top-8 right-8">
+          <div className="md:absolute md:top-8 md:right-8">
             <button
               className="contact"
               onClick={() => {
@@ -52,15 +62,17 @@ export const Overlay = () => {
               Me contacter
             </button>
           </div>
-          <div className="flex flex-col items-center md:flex-row md:justify-between">
-            <div>NAMUR, BE & QC, CA</div>
-            <div>
-              <a to={"mailto:bastien.daxhelet@gmail.com"}>
-                BASTIEN.DAXHELET@GMAIL.COM
-              </a>
-            </div>
-            <div>
-              <a to={"tel:+32 472 09 18 22"}>+32 472 09 18 22</a>
+          <div className="absolute bottom-0 w-full">
+            <div className="md:flex text-center mx-8 mb-8 text-white justify-center md:justify-between md:items-center">
+              <div>[ Québec, Canada ]</div>
+              <div>
+                <a href={"mailto:bastien.daxhelet@gmail.com"}>
+                  [ bastien.daxhelet@gmail.com ]
+                </a>
+              </div>
+              <div>
+                <a href={"tel:+1 367 380 3028"}>[ 367-380-3028 ]</a>
+              </div>
             </div>
           </div>
         </>
