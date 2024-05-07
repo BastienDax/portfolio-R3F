5868d5d562e4a9f8b10bdb59206ecb565d939131
Bastien Daxhelet <bastos@MacBook-Pro-de-Bastien.local>
2024-05-07 15:20:52 -0400

    add page interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,19 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Overlay } from "./components/Overlay";
-import { ScrollControls, useScroll } from "@react-three/drei";
-import { Deskroom } from "./components/Deskroom";
-import { getProject, val } from "@theatre/core";
-import {
-  SheetProvider,
-  PerspectiveCamera,
-  useCurrentSheet,
-} from "@theatre/r3f";
-
-import deskroomThroughState from "./deskroom.json";
-import { useRef } from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Interface from "./pages/Interface";
 
 function App() {
-  const sheet = getProject("Portfolio", { state: deskroomThroughState }).sheet(
-    "Scene"
-  );
-
-  const [animationComplete, setAnimationComplete] = useState(false);
-  const navigationRef = useRef(null);
-
-  useEffect(() => {
-    if (animationComplete) {
-      navigationRef.current.click();
-    }
-  }, [animationComplete]);
-
-  const handleAnimationComplete = () => {
-    setAnimationComplete(true);
-  };
-
   return (
     <>
-      <Canvas flat gl={{ preserveDrawingBuffer: true }}>
-        <ScrollControls pages={3} damping={1} maxSpeed={1}>
-          <SheetProvider sheet={sheet}>
-            <Scene onAnimationComplete={handleAnimationComplete} />
-          </SheetProvider>
-        </ScrollControls>
-      </Canvas>
-
-      <a ref={navigationRef} href="./interface" style={{ display: "none" }} />
-
-      <Overlay />
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/interface" element={<Interface />} />
+          <Route path="/*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 
 export default App;
-
-function Scene({ onAnimationComplete }) {
-  const sheet = useCurrentSheet();
-  const scroll = useScroll();
-
-  useFrame(() => {
-    const sequenceLength = val(sheet.sequence.pointer.length);
-
-    sheet.sequence.position = scroll.offset * sequenceLength;
-
-    if (sheet.sequence.position >= sequenceLength - 1) {
-      onAnimationComplete();
-    }
-  });
-
-  return (
-    <>
-      <color attach="background" args={["#688CAE"]} />
-
-      <PerspectiveCamera
-        theatreKey="Camera"
-        makeDefault
-        position={[0, 0, 0]}
-        fov={75}
-        near={0.1}
-        far={70}
-      />
-
-      <Deskroom />
-    </>
-  );
-}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,84 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Overlay } from "../components/Overlay";
+import { ScrollControls, useScroll } from "@react-three/drei";
+import { Deskroom } from "../components/Deskroom";
+import { getProject, val } from "@theatre/core";
+import {
+  SheetProvider,
+  PerspectiveCamera,
+  useCurrentSheet,
+} from "@theatre/r3f";
+
+import deskroomThroughState from "../deskroom.json";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function Home() {
+  const sheet = getProject("Portfolio", { state: deskroomThroughState }).sheet(
+    "Scene"
+  );
+
+  const [animationComplete, setAnimationComplete] = useState(false);
+  const navigationRef = useRef(null);
+
+  useEffect(() => {
+    if (animationComplete) {
+      navigationRef.current.click();
+    }
+  }, [animationComplete]);
+
+  const handleAnimationComplete = () => {
+    setAnimationComplete(true);
+  };
+
+  return (
+    <>
+      <Canvas flat gl={{ preserveDrawingBuffer: true }}>
+        <ScrollControls pages={3} damping={1} maxSpeed={1}>
+          <SheetProvider sheet={sheet}>
+            <Scene onAnimationComplete={handleAnimationComplete} />
+          </SheetProvider>
+        </ScrollControls>
+      </Canvas>
+
+      <a ref={navigationRef} href="./interface" style={{ display: "none" }} />
+
+      <Overlay />
+    </>
+  );
+}
+
+export default Home;
+
+function Scene({ onAnimationComplete }) {
+  const sheet = useCurrentSheet();
+  const scroll = useScroll();
+
+  useFrame(() => {
+    const sequenceLength = val(sheet.sequence.pointer.length);
+
+    sheet.sequence.position = scroll.offset * sequenceLength;
+
+    if (sheet.sequence.position >= sequenceLength - 1) {
+      onAnimationComplete();
+    }
+  });
+
+  return (
+    <>
+      <color attach="background" args={["#688CAE"]} />
+
+      <PerspectiveCamera
+        theatreKey="Camera"
+        makeDefault
+        position={[0, 0, 0]}
+        fov={75}
+        near={0.1}
+        far={70}
+      />
+
+      <Deskroom />
+    </>
+  );
+}
