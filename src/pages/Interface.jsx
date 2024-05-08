5ad2db06e07893c913b91bdfb058ea43670d2263
Bastien Daxhelet <bastos@MacBook-Pro-de-Bastien.local>
2024-05-08 18:35:28 -0400

    add more interface

--- src/pages/Interface.jsx
+++ src/pages/Interface.jsx
@@ -41,86 +41,75 @@ export default function Interface() {
   };
 
   return (
-    <Canvas>
-      <FaceLandmarker>
-        <Environment
-          files="models/photostudio.hdr"
-          background
-          resolution={256}
-        />
+    <>
+      <Canvas>
+        <FaceLandmarker>
+          <Environment
+            files="models/photostudio.hdr"
+            background
+            resolution={256}
+          />
 
-        {cameraAccepted ? (
-          <FaceControls ref={face} offsetScalar={50} />
-        ) : (
-          <>
-            <PerspectiveCamera makeDefault near={0.1} far={2000} fov={75} />
+          {cameraAccepted ? (
+            <FaceControls ref={face} offsetScalar={50} />
+          ) : (
+            <>
+              <PerspectiveCamera makeDefault near={0.1} far={2000} fov={75} />
 
-            <OrbitControls />
-          </>
-        )}
+              <OrbitControls />
+            </>
+          )}
 
-        {choiceCamera === false && (
-          <Html center position={[2, 0, -25]}>
-            <div className="w-screen h-screen flex justify-center">
-              <div className="flex mt-96 flex-col justify-center items-center">
-                <div className="flex justify-center gap-x-4">
-                  <button className="contact" onClick={() => acceptCamera()}>
-                    Accepter
-                  </button>
-                  <button className="contact" onClick={() => deniedCamera()}>
-                    Refuser
+          {choiceCamera === false && (
+            <Html center position={[2, 0, -25]}>
+              <div className="w-screen h-screen flex justify-center">
+                <div className="flex mt-96 flex-col justify-center items-center">
+                  <div className="flex justify-center gap-x-4">
+                    <button className="contact" onClick={() => acceptCamera()}>
+                      Accepter
+                    </button>
+                    <button className="contact" onClick={() => deniedCamera()}>
+                      Refuser
+                    </button>
+                  </div>
+                  <p className="mt-14 text-xl font-bold text-white">
+                    Acceptez l’usage de la caméra pour avoir une meilleure
+                    expérience.
+                  </p>
+                </div>
+              </div>
+              <div className="absolute bottom-4 left-0 w-full">
+                <div className="text-center">
+                  <p className="text-white">
+                    <b>Bastien Daxhelet</b> - Développeur créatif
+                  </p>
+                </div>
+              </div>
+            </Html>
+          )}
+
+          {choiceCamera === true && !tutorial && (
+            <Html center position={[2, 0, -25]}>
+              <div className="w-screen h-screen flex justify-center">
+                <div className="flex flex-col justify-center items-center">
+                  <div className="">
+                    <FaceSVGAnimation />
+                  </div>
+                  <p className="mt-14 text-xl font-bold text-white">
+                    Bougez votre tête pour découvrir votre environnement
+                  </p>
+                  <button
+                    className="contact mt-12"
+                    onClick={() => tutorialEnd()}
+                  >
+                    Compris
                   </button>
                 </div>
-                <p className="mt-14 text-xl font-bold text-white">
-                  Acceptez l’usage de la caméra pour avoir une meilleure
-                  expérience.
-                </p>
               </div>
-            </div>
-            <div className="absolute bottom-4 left-0 w-full">
-              <div className="text-center">
-                <p className="text-white">
-                  <b>Bastien Daxhelet</b> - Développeur créatif
-                </p>
-              </div>
-            </div>
-          </Html>
-        )}
-
-        {choiceCamera === true && !tutorial && (
-          <Html center position={[2, 0, -25]}>
-            <div className="w-screen h-screen flex justify-center">
-              <div className="flex flex-col justify-center items-center">
-                <div className="">
-                  <FaceSVGAnimation />
-                </div>
-                <p className="mt-14 text-xl font-bold text-white">
-                  Bougez votre tête pour découvrir votre environnement
-                </p>
-                <button className="contact mt-12" onClick={() => tutorialEnd()}>
-                  Compris
-                </button>
-              </div>
-            </div>
-          </Html>
-        )}
-
-        {choiceCamera === true && tutorial && cameraAccepted && (
-          <group position={[2, 0, -25]}>
-            <Html transform>
-              <Radio2 />
             </Html>
-            <Html transform position={[0, -10.5, 5]}>
-              <BottomMenu />
-            </Html>
-            <Html transform position={[-17, 0, 10]}>
-              <SideMenu />
-            </Html>
-          </group>
-        )}
+          )}
 
-        {cameraAccepted === false && tutorial && (
-          <Float rotationIntensity={0.2}>
+          {choiceCamera === true && tutorial && cameraAccepted && (
             <group position={[2, 0, -25]}>
               <Html transform>
                 <Radio2 />
@@ -132,46 +121,109 @@ export default function Interface() {
                 <SideMenu />
               </Html>
             </group>
-          </Float>
-        )}
+          )}
 
-        <DeviceOrientationControls />
-      </FaceLandmarker>
-    </Canvas>
+          {cameraAccepted === false && tutorial && (
+            <Float rotationIntensity={0.2}>
+              <group position={[2, 0, -25]}>
+                <Html transform>
+                  <Radio2 />
+                </Html>
+                <Html transform position={[0, -10.5, 5]}>
+                  <BottomMenu />
+                </Html>
+                <Html transform position={[-17, 0, 10]}>
+                  <SideMenu />
+                </Html>
+              </group>
+            </Float>
+          )}
 
-    // <Canvas>
-    //   <Environment files="models/photostudio.hdr" background />
+          <DeviceOrientationControls />
+        </FaceLandmarker>
+      </Canvas>
 
-    //   <OrbitControls />
-
-    //   <Html center>
-    //     <div className="w-screen h-screen flex justify-center">
-    //       <div className="flex justify-center items-center">
-    //         <p className="text-xl font-bold text-white">
-    //           Acceptez l’usage de la caméra pour découvrir l’expérience
-    //         </p>
-    //       </div>
-    //     </div>
-    //     <div className="absolute bottom-4 left-0 w-full">
-    //       <div className="text-center">
-    //         <p className="text-white">
-    //           <b>Bastien Daxhelet</b> - Développeur créatif
-    //         </p>
-    //       </div>
-    //     </div>
-    //   </Html>
-
-    // <group position={[2, 0, -25]}>
-    //   <Html transform>
-    //     <Radio2 />
-    //   </Html>
-    //   <Html transform position={[0, -7.5, 5]}>
-    //     <BottomMenu />
-    //   </Html>
-    //   <Html transform position={[-17, 0, 10]}>
-    //     <SideMenu />
-    //   </Html>
-    // </group>
-    // </Canvas>
+      <div className="h-full w-screen">
+        <div className="absolute bottom-4 right-4">
+          <div>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider pointer-events-auto">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                >
+                  <g clip-path="url(#clip0_368_1621)">
+                    <path
+                      d="M1.3125 4.44718V4.39944C1.3125 2.80112 2.60825 1.50537 4.20657 1.50537H4.25431"
+                      stroke="white"
+                      stroke-width="1.3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M17.8012 15.0527V15.1005C17.8012 16.6988 16.5054 17.9945 14.9071 17.9945H14.8594"
+                      stroke="white"
+                      stroke-width="1.3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M1.3125 15.0527V15.1005C1.3125 16.6988 2.60825 17.9945 4.20657 17.9945H4.25431"
+                      stroke="white"
+                      stroke-width="1.3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M14.8594 1.50537H14.9071C16.5054 1.50537 17.8012 2.80112 17.8012 4.39944V4.44718"
+                      stroke="white"
+                      stroke-width="1.3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M6.1155 5.53516H6.09357C5.6115 5.53516 5.2207 5.92595 5.2207 6.40802V6.96736C5.2207 7.44943 5.6115 7.84022 6.09357 7.84022H6.1155C6.59758 7.84022 6.98837 7.44943 6.98837 6.96736V6.40802C6.98837 5.92595 6.59758 5.53516 6.1155 5.53516Z"
+                      fill="white"
+                    />
+                    <path
+                      d="M13.0218 5.53516H12.9998C12.5177 5.53516 12.127 5.92595 12.127 6.40802V6.96736C12.127 7.44943 12.5177 7.84022 12.9998 7.84022H13.0218C13.5038 7.84022 13.8946 7.44943 13.8946 6.96736V6.40802C13.8946 5.92595 13.5038 5.53516 13.0218 5.53516Z"
+                      fill="white"
+                    />
+                    <path
+                      d="M6.60938 14.3721C7.38934 15.1914 8.42349 15.6291 9.55764 15.6291C10.6918 15.6291 11.7259 15.1914 12.5059 14.3721"
+                      stroke="white"
+                      stroke-width="1.3"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                    />
+                    <path
+                      d="M9.6106 8.73828L10.2922 9.41987C10.3735 9.50115 10.3735 9.63308 10.2922 9.71469L8.99805 11.0088"
+                      stroke="white"
+                      stroke-width="1.3"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_368_1621">
+                      <rect
+                        width="19"
+                        height="19"
+                        fill="white"
+                        transform="translate(0.0585938 0.25)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </span>
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
