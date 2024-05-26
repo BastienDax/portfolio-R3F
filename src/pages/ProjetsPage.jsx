3f9734643130e13f7bdd543f579fd6e1d1e32195
Bastien Daxhelet <bastos@app1153rdcavant.localdomain>
2024-05-25 23:47:36 -0400

    responsive projet page

--- src/pages/ProjetsPage.jsx
+++ src/pages/ProjetsPage.jsx
@@ -58,26 +58,26 @@ export default function ProjetsPage({ onClick }) {
               </button>
             </div>
 
-            <div className=" mb-20 pb-12 h-full text-white overflow-auto">
-              <div className="flex bg-projets px-16 text-black">
-                <div className="mt-40 mb-20 w-2/3 flex flex-col">
+            <div className="mb-20 pb-12 h-full text-white overflow-auto">
+              <div className="flex bg-projets px-8 sm:px-16 text-black">
+                <div className="mt-40 mb-20 w-full sm:w-2/3 flex flex-col">
                   <p className="mb-2 text-sm">Projet à la une</p>
                   <p className="mb-2 font-bold text-4xl">FillUP</p>
-                  <p className="w-3/4">
+                  <p className="w-full sm:w-3/4">
                     J'ai été mandaté par cette startup pour développer une
                     application web destinée à réduire les files d'attente dans
                     les bars et lors des événements.
                   </p>
-                  <a className="btn-projets" href="#projets">
+                  <a className="btn-projets mb-20 sm:mb-0" href="#projets">
                     En voir plus
                   </a>
                 </div>
               </div>
-              <div className="mt-8 pl-16">
-                <p id="projets" className="mb-8 font-bold text-4xl">
+              <div className="mt-8 pl-8 sm:pl-16">
+                <p id="projets" className="mb-8 font-bold text-3xl sm:text-4xl">
                   Mes projets clients
                 </p>
-                <div className="overflow-x-auto whitespace-nowrap flex gap-x-12 gap-y-32 h-52">
+                <div className="-ml-8 sm:-ml-16 px-8 sm:px-16 overflow-x-auto whitespace-nowrap flex gap-x-12 gap-y-32 h-52">
                   <div className="max-w-80 h-36 rounded-lg flex justify-center bg-fillup">
                     <button
                       onClick={() => setCurrentPage(1)}
